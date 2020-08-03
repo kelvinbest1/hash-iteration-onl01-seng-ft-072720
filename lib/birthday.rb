3472fd3c1f6f6e1bf10f4ea535a9e8082be0028f
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-03 20:52:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,6 @@ def age_appropriate_birthday(birthday_kids)
     if birthday_kids<=12 puts "Happy Birthday #{name}! You are now #{age} years old!"
     else put "You are too old for this"
     end
-  
+  end
 
   
